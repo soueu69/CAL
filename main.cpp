ff1e87379d0632a5a11a08d3045e8e22e6c896ab
soueu69 <65418925+soueu69@users.noreply.github.com>
2021-05-17 10:09:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,21 @@
 #include <fstream>
 using namespace std;
 
+void initialize(){
+    cout<<endl;
+    cout<<"....................................................................................................."<<endl;
+    cout<<"Loading finished!"<<endl;
+    cout<<endl;
+    cout<<"VaccineRouter..."<<endl;
+    cout<< "................."<< "MENU"<< "................."<<endl;
+    cout<<"[0]-Change map setup"<<endl<<"[1]-Program route"<<endl;
+}
+
 int main() {
+    Graph graph;
     cout<<"........................................................................................."<<endl;
     cout<<"LOADING GRAPH..."<<endl;
     cout<<"........................................................................................."<<endl;
-    Graph graph;
     ///READ NODE FILE
     string textline;
     ifstream infile;
@@ -33,8 +43,10 @@ int main() {
             info.erase(info.begin() + 5);
         }
         int int_1 = stoi(info[0]);
-        int int_2 = stoi(info[2]);
-        int int_3 = stoi(info[3]);
+        double int_2 = stod(info[2]);   // coordanate x
+        double int_3 = stod(info[3]);   //coordenate y
+        //cout<<int_2<<endl;
+        //cout<<int_3<<endl;
         Node *objeto = new Node(int_1, info[4], info[1], int_2, int_3);
         //nodes.push_back(objeto);
         graph.add_nodes(objeto);
@@ -77,7 +89,7 @@ int main() {
         int node1 = stoi(linha[1]);
         int node2 = stoi(linha[2]);
 
-        cout<< "THIS EDGE-> "<<"NODE 1: "<<  node1 << "   NODE 2: "<< node2<<endl;
+        //cout<< "THIS EDGE-> "<<"NODE 1: "<<  node1 << "   NODE 2: "<< node2<<endl;
 
 
         Edge *objeto = new Edge(node1, node2, id1, linha[3]);
@@ -89,8 +101,6 @@ int main() {
         cout<< objeto->get_2ID()<<endl;
         cout<<"------------------"<<endl;
          */
-
-
 
         for (int i = 0; i < graph.get_size(); i++) {
             int value = graph.get_node(i)->get_ID();    //NODE ID
@@ -111,25 +121,31 @@ int main() {
     for (int x = 0; x < graph.get_size(); x++) {
         cout<<"----------------------"<<endl;
         cout<<"MAIN NODE: "<<graph.get_node(x)->get_name()<<endl;
+        //cout<<"COORDINATES:"<<graph.get_node(x)->get_x()<<","<<graph.get_node(x)->get_y()<<endl;
         cout<<"NUMBER OF OUTGOING EDGES: " <<graph.get_node(x)->outgoing_edges_size()<<endl;
         for(int a=0;a<graph.get_node(x)->outgoing_edges_size();a++){
-            cout<<"EDGE: "<<graph.get_node(x)->get_edge(a)->get_name()<<endl;
-            cout<<"EDGE CONECTED NODE1 :"<<graph.get_node(x)->get_edge(a)->get_node1()->get_ID()<<endl;
-            cout<<"EDGE CONECTED NODE2 :"<<graph.get_node(x)->get_edge(a)->get_node2()->get_ID()<<endl;
-        }
+            //cout<<"EDGE: "<<graph.get_node(x)->get_edge(a)->get_name()<<endl;
+            cout<<"LENGHT: "<<graph.get_node(x)->get_edge(a)->calculate_distance()<<endl;
+            //cout<<"EDGE CONECTED NODE1 :"<<graph.get_node(x)->get_edge(a)->get_node1()->get_ID()<<endl;
+            //cout<<"EDGE CONECTED NODE2 :"<<graph.get_node(x)->get_edge(a)->get_node2()->get_ID()<<endl;
 
+        }
         //just checking if everything is good ...
 
     }
+    initialize();
+    int choose;
     cout<<endl;
-    cout<<"....................................................................................................."<<endl;
-    cout<<"Loading finished!"<< " EZ PZ "<< " ... CATS>CAL-> IS SHIT!! %""";
+    cout<<"Available vaccination centers:"<<endl;
+    cout<<endl;
+    for(int a =0; a<graph.get_size();a++){
+        if(graph.get_node(a)->get_importance()=="Application"){
+            cout<<graph.get_node(a)->get_name();
+            cout<< "->" <<"ID:"<<graph.get_node(a)->get_ID()<<endl;
 
+    }
 
-
-
-
-
+    }
 
 }
 
