00ef83cec77b3750daa570f3724fbb2f50fbc6ee
soueu69 <65418925+soueu69@users.noreply.github.com>
2021-05-17 10:10:52 +0100

    Add files via upload

---
+++ Vehicle.h
@@ -0,0 +1,31 @@
+//
+// Created by diogo on 16/05/2021.
+//
+
+#ifndef UNTITLED21_VEHICLE_H
+#define UNTITLED21_VEHICLE_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <utility>
+
+#include <limits>
+#include <cmath>
+
+class Vehicle{
+    private:
+         int ID;
+         int max_quantity;
+    public:
+         int get_ID(){
+             return ID;
+         }
+
+         int get_max_quantity(){
+             return ID;
+         }
+};
+
+#endif //UNTITLED21_VEHICLE_H
