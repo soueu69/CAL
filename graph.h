1efd2588eb625b6ffdcaf87a720187768c35c084
soueu69 <65418925+soueu69@users.noreply.github.com>
2021-05-21 11:01:40 +0100

    Update graph.h

--- graph.h
+++ graph.h
@@ -22,15 +22,20 @@ class Edge;
 
 class Vertex{
 private:
+    int IDaux;   //auxiliary for updating graph
+    int menuID=599;  // menu aux
+  
+
+    bool available =true;  //for checking if it can be used
     string importance;
     string name;
     vector<Edge*> outgoing_edges;
     double double_x;
     double double_y;
-    int menuID;  // menu aux
 
-    Vertex *prev;  //auxiliary
+
     double dist;   //auxiliary
+    Vertex * previous; //auxiliary
 public:
 
     Vertex(string imp,double xx,double yy,string nam){
@@ -39,7 +44,22 @@ public:
         double_y=yy;
         name=nam;
     }
-    int set_menuID(int k){
+    void set_IDaux(int x){
+        IDaux=x;
+    }
+    int get_IDaux(){
+        return IDaux;
+    }
+
+    bool get_availablility(){
+        return available;
+    }
+
+    void set_availablility(bool flag){
+        available=flag;
+    }
+
+    void set_menuID(int k){
         menuID=k;
     }
 
@@ -50,10 +70,24 @@ public:
     string get_importance(){
         return importance;
     }
-
-    void set_prev(Vertex * dis){
-        prev=dis;
+    /*
+    void set_visited(bool dis){
+        visited=dis;
     }
+
+
+    bool get_visited(){
+            return visited;
+    };
+     */
+    Vertex * get_previous(){
+        return previous;
+    }
+
+    void set_previous(Vertex * obj){
+        previous=obj;
+    }
+
     void set_dist(double distance){
         dist=distance;
     }
@@ -112,37 +146,55 @@ public:
         return vertexes[i];
     }
 
-    void dijkstraShortestPath(Vertex * source);
+    vector<Vertex*> dijkstraShortestPath(Vertex * source,Vertex * destiny);
 };
 
-void Graph::dijkstraShortestPath(Vertex * source) {
-    vector<Vertex*> lista;            // criar lista com todos os nodes
+vector<Vertex*> Graph::dijkstraShortestPath(Vertex * source,Vertex * destiny) {
+    vector<Vertex*> visited;
+    vector<Vertex*> lista;             // criar lista com todos os nodes
     for(auto vertices : vertexes){     // percorrer todos os nodes e atualizar valores
         vertices->set_dist(INT_MAX);
-        vertices->set_prev(NULL);
         lista.push_back(vertices);   // colocamos todos os nodes neste vetor
     }
     source->set_dist(0);
 
-    while(!lista.empty()){                              // enquanto houver nodes na lista
-        auto node = lista[0];                           // node é o node inicial
+
+    while(!lista.empty()){
+        auto node = lista[0];
         int index=0;
-        for(int i=1; i<lista.size();i++){                // percorrer todos os outros nodes da lista
-            if(node->get_dist()>lista[i]->get_dist()){   // se algum desses nodes tiver uma distancia menor é escolhida
+        for(int i=1; i<lista.size();i++){
+            if((node->get_dist()>lista[i]->get_dist())){
                 node=lista[i];
-                index=i;
+                index=i;                                   // escolhemos o node com menor distancia de todos os nossos nodes(o node source comeca a 0 logo é escolhido)
             }
         }
+        //node->set_visited(true);
+        visited.push_back(node);                            // colocamos o node com menor distancia no vetor de visitados
         lista.erase(lista.begin()+index);                 // retira da lista o node com menor distancia percorrida e passo a trabalhar com este node
 
-        for(auto outgoing_edges : node->get_outgoing_edges()){                           // todas as edges que saiem do node que escolhemos por ter menor distancia
+        for(auto outgoing_edges : node->get_outgoing_edges()){            // todas as edges que saiem do node que escolhemos por ter menor distancia
             double total_percorrido =  node->get_dist() + outgoing_edges->get_lenght();  // distancia para cada um dos nodes é a distancia do node anterior e somo o amanho da edge
             if(total_percorrido<outgoing_edges->get_destiny_vertex()->get_dist()){
                 outgoing_edges->get_destiny_vertex()->set_dist(total_percorrido);
-                outgoing_edges->get_destiny_vertex()->set_prev(node);
+                outgoing_edges->get_destiny_vertex()->set_previous(node);
+
             }
         }
     }
+
+    ///return path;
+    auto atual = destiny;                   // node destino final
+    vector<Vertex*> path;
+    while(atual!=source){
+        path.push_back(atual);              //path comeca com o destino pretendido
+        for(auto node :visited){
+            if(atual==node){                    //procuramos o desntino no vetor de node visitados
+                atual=atual->get_previous();      //quando é encontrado o node atual, subsitutimos pelo anterior pretendido
+            }
+        }
+    }
+    path.push_back(source);
+    return path;
 }
 
 #endif //PROJETO_CAL_GRAPH_H
